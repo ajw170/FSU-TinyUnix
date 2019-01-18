e80de988acdb6f31dca56792ff03521c36244428
Andrew Wood <wood.andrew9@gmail.com>
2019-01-18 17:57:52 -0500

    Interactive mode working.

--- shell.c
+++ shell.c
@@ -10,9 +10,11 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 //function prototype
-void parse (char *, char **);
+void parse (char *, char **, const char *);
 
 int main()
 {
@@ -22,43 +24,72 @@ int main()
 
   int BUFSIZE = 1024;
   char line[BUFSIZE];
-  char *argv[64]; //max arguments is 64
+  char * commandv[1024] = {NULL}; //1024 possible separate commands
+  char * argv[64] = {NULL}; //max arguments is 64
+  static char * commandDelim = ";\n";
+  static char * subDelim = " \n\t";
+  bool quitTrigger = 0;
 
   // display the prompt
   while (1)
   {
+    if (quitTrigger)
+      exit(0); //quit program
     printf("Prompt>");
     fflush(stdout);
-    if (fgets(line, BUFSIZE, stdin) == NULL)
+    if (fgets(line, BUFSIZE, stdin) == NULL) //uses existing line buffer as memory store
     {
-      printf("There was a problem with the input.");
+      perror("The following error occurred");
     }
     else //valid entry, now parse the arguments
     {
-      parse(line,argv);
-      pid = fork();
-      if (pid == 0) //inside child process
+      parse(line,commandv,commandDelim);
+      //the line now is separated by individual commands between semicolons, continue processing each one
+
+      //decipher individual arguments on the line
+      size_t count = 0;
+      while (commandv[count] != NULL)
       {
-        execvp(argv[0],argv);
-        fprintf(stderr,"Sorry, but the execution failed.\n");
-      }
-      else //inside parent process
-      {
-        c = wait(&cstatus); //wait for child to complete
-        printf("PID %d exited with status %d\n", (long) c, cstatus);
+        parse(commandv[count],argv,subDelim);
+        if (argv[0] == NULL)
+        {
+          ++count;
+          memset(argv,0,sizeof(char *) * 64);
+          continue;
+        }
+        if (strcmp(argv[0],"quit") == 0)
+        {
+          ++count;
+          quitTrigger = 1; //indicates the program should quit
+          continue;
+        }
+        pid = fork();
+        if (pid == 0) //inside child process
+        {
+          execvp(argv[0],argv);
+          fprintf(stderr,"Exec failed on command %s.\n",argv[0]);
+          exit(1);
+        }
+        else //inside parent process
+        {
+          c = wait(&cstatus); //wait for child to complete
+          printf("PID %d exited with status %d\n", (int) c, cstatus);
+        }
+        ++count;
+        //clear argv vector to ensure it doesn't affect next read
+        memset(argv,0,sizeof(char *) * 64);
       }
     }
   }
 }
 
-void parse(char * line, char ** argv)
+void parse(char * line, char ** separator, const char * delim)
 {
-  static char * delimiter = " \n\t";
-  char * token = strtok(line, delimiter);  //token is a pointer to a character sequence terminated in '\0'
+  char * token = strtok(line, delim);  //token is a pointer to a character sequence terminated in '\0'
   while (token != NULL)
   {
-    *argv++ = token;
-    token = strtok(NULL, delimiter);
+    *separator++ = token;
+    token = strtok(NULL, delim);
   }
-  *argv = (char *)'\0';                 /* mark the end of argument list  */
+  *separator = (char *)'\0';                 /* mark the end of argument list  */
 }
